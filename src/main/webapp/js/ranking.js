6e86989273dab99117273c6315810935dfce6927
kttling2005 <80590767+kttling2005@users.noreply.github.com>
2026-09-26 14:17:24 +0700

    Add files via upload

---
+++ src/main/webapp/js/ranking.js
@@ -0,0 +1,12 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
+ */
+
+
+function goBack() {
+
+    window.location.href =
+        "home";
+
+}
---
+++ src/main/webapp/js/result.js
@@ -0,0 +1,19 @@
+function playAgain() {
+
+    window.location.href = "/Caro_game/game-play";
+
+}
+
+
+function viewRanking() {
+
+    window.location.href = "/Caro_game/ranking";
+
+}
+
+
+function goHome() {
+
+    window.location.href = "/Caro_game/home";
+
+}
